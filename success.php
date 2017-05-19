0bd8c870b00084c1a2c7e3c02de5ae774eda945b
HyperQing <469379004@qq.com>
2017-05-19 14:05:04 +0800

    更新跳转页面样式

--- success.php
+++ success.php
@@ -6,6 +6,11 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>操作成功！</title>
+  <style>
+    body {
+      font-family: "Microsoft YaHei UI Light", sans-serif;
+    }
+  </style>
 </head>
 <body>
 <h1>:) <?php echo isset($info) ? $info : '未知错误'; ?></h1>
