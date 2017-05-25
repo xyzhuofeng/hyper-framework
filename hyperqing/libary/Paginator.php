368531bb83876e3840a0286f3b479eeddd5d988f
HyperQing <469379004@qq.com>
2017-05-25 21:42:43 +0800

    分页类实现数组方法访问接口，现在可以用[]访问数据集了

--- hyperqing/libary/Paginator.php
+++ hyperqing/libary/Paginator.php
@@ -7,7 +7,7 @@ namespace hyper;
  * @author HyperQing
  * 实现迭代器接口
  */
-class Paginator implements \Iterator
+class Paginator implements \Iterator, \ArrayAccess
 {
     /**
      * @var array 数据集
@@ -175,5 +175,70 @@ STR;
     public function rewind()
     {
         $this->position = 0;
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+        if (isset($this->data[$offset])) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+        return $this->data[$offset];
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
     }
 }
