0e24d7c09b70711b75679d376d8a330485b089d0
HyperQing <469379004@qq.com>
2017-05-25 21:18:18 +0800

    添加跳转页面模板

---
+++ hyperqing/tpl/success.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>操作成功！</title>
+  <style>
+    body {
+      font-family: "Microsoft YaHei UI Light", sans-serif;
+    }
+  </style>
+</head>
+<body>
+<h1>:) <?php echo isset($info) ? $info : '未知错误'; ?></h1>
+<h3>
+  <span id="wait_time">
+        <?php echo isset($wait_time) ? $wait_time : 3; ?>
+  </span> 秒后返回，点击 <a href="<?php echo isset($url) ? $url : '#'; ?>">返回</a>
+</h3>
+<script>
+  var time = <?php echo isset($wait_time) ? $wait_time : 3; ?>;
+  var url = '<?php echo isset($url) ? $url : '#';?>';
+  var interval = setInterval(function () {
+    time -= 1;
+    document.getElementById('wait_time').innerHTML = time;
+    if (time === 0) {
+      clearInterval(interval);
+      window.location.href = url;
+    }
+  }, 1000);
+</script>
+</body>
+</html>
---
+++ hyperqing/tpl/update.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * 新增联系人
+ */
+
+require_once __DIR__ . '/hyperqing.php';
+// 检查请求类型
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    error('非法操作');
+}
+
+// 读入并过滤表单
+$id = isset($_POST['id']) ? $_POST['id'] : null;
+$name = isset($_POST['name']) ? $_POST['name'] : null;
+$phone = isset($_POST['phone']) ? $_POST['phone'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+// 联系人必填
+$name = addslashes(htmlspecialchars(trim($name)));
+if (!$name || $name == '') {
+    error('姓名不能为空');
+}
+// 提交了手机的情况，检查纯数字、长度
+if ($phone && $phone != '') {
+    if (!filter_var($phone, FILTER_VALIDATE_INT)) {
+        error('手机格式错误');
+    }
+    if (strlen($phone) != 11) {
+        error('手机长度为11位');
+    }
+}
+// 提交了邮箱的情况，检查邮箱格式
+if ($email && $email != '') {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        error('邮箱格式错误');
+    }
+}
+// 更新数据
+$data = [
+    'name' => $name,
+    'phone' => $phone,
+    'email' => $email
+];
+$row = Contacts::update($id, $data);
+if ($row) {
+    success('成功保存 ' . $row . ' 条记录','./index.php');
+}
+error('保存失败');
